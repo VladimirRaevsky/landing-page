e55db758dc5d5f2aaedc4c287ed661b0a633235c
vladimir r <rayevskiy.volodya@mail.ru>
2021-06-03 16:57:21 +0300

    made a section with video

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const swiper1 = document.querySelector('.slider-container');
+
+let swiperSlider1 = new Swiper(swiper1, {
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	loop: true,
+	spaceBetween: 105,
+});
